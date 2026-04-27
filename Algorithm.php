116ff734054b9546eb0ff90789eac76be2ac0cdb
Andrew Maina <mainaandrewcom@gmail.com>
2026-04-27 08:50:35 +0300

    Fix binary search condition for left and right pointers

--- Algorithm.php
+++ Algorithm.php
@@ -12,11 +12,11 @@ function binarysearch($arr, $target)
         if ($arr[$mid]===$target){
             return $mid;
         }
-        if ($arr[$mid]> $target){
-            $right = $mid + 1;
+        if ($arr[$mid] < $target){
+            $left = $mid + 1;
         }
         else{
-            $left = $mid - 1;
+            $right = $mid - 1;
         }    
     }
     return -1;
